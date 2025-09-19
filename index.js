8fb921e306211b0545e5446b583bf4e595ba9145
Pouria <108181964+pouriavj@users.noreply.github.com>
2025-09-19 21:36:41 +0330

    Update styles

--- index.js
+++ index.js
@@ -5,47 +5,49 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 const API_URL = "https://api.frankfurter.dev/v1/latest";
-const money = "Select pair"
+
+let money = "Select a pair";
+let currencyFrom = "USD";
+let currencyTo = "EUR";
+let loading = false;
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", async (req, res) => {
-
-    try {
-    const result = await axios.get(API_URL + "?base=USD&symbols=EUR");
-    
-    
-    res.render("index.ejs", { currency: result.data.rates.EUR});
-  } catch (error) {
-    console.log(error.message);
-    
-    
-    res.status(500);
-  }
-
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    currency: money,
+    currencyFrom,
+    currencyTo,
+    loading,
+  });
 });
+
 app.post("/", async (req, res) => {
-    
-    console.log(req.body.currencyFrom);
-    if (req.body.currencyFrom === req.body.currencyTo){
-        res.render("index.ejs", { currency: 1});
-    }
-    else{
-    try {
-    const result = await axios.get(API_URL + `?base=${req.body.currencyFrom}&symbols=${req.body.currencyTo}` );
-   
-    console.log(result.data.rates);
-    
-    res.render("index.ejs", { currency: result.data.rates[`${req.body.currencyTo}`]});
+  currencyFrom = req.body.currencyFrom;
+  currencyTo = req.body.currencyTo;
+
+  if (currencyFrom === currencyTo) {
+    money = 1;
+    return res.redirect("/");
+  }
+
+  try {
+    loading = true;
+    const result = await axios.get(
+      `${API_URL}?base=${currencyFrom}&symbols=${currencyTo}`
+    );
+
+    money = result.data.rates[currencyTo];
+    loading = false;
+    res.redirect("/");
   } catch (error) {
     console.log(error.message);
-    
-    
-    res.status(500);
-  }
+    money = "Error fetching data";
+    loading = false;
+    res.redirect("/");
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
